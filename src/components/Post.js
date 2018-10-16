b4d964747176209f3bb82a515344e9e478aa6742
Christina Holland <hsubox@gmail.com>
2018-10-16 00:39:36 +0000

    Image tags and formatting

--- src/components/Post.js
+++ src/components/Post.js
@@ -4,7 +4,6 @@ import 'firebase/firestore';
 import { format } from 'date-fns';
 import get from 'lodash/get';
 import { LOADING_STATUS, STANDARD_DATE_FORMAT } from '../utils/constants';
-import { escapeRegExp } from '../utils/dbhelpers';
 
 class Post extends Component {
 	constructor() {
@@ -93,19 +92,64 @@ class Post extends Component {
 				</form>
 			);
 		}
-		const lines = content.split('\n');
-		const imageRE = new RegExp(/\[img\](.+)\[\/img\]/g);
-		return lines.map((line, index) => {
-			const imgParts = line.split(/(\[img\].+\[\/img\])/g);
-			return imgParts.map((part) => {
-				if (part.substr(0, 5) === '[img]') {
-					return <img src={part.replace(imageRE, '$1')} />
-				} else if (part) {
-					return (<p key={index} className="content-line">{part}</p>);
-				}
-				return null;
-			});
-		});
+		const rawTokens = content.split(/(\[\/?(?:spoiler|img|b|i)\])/);
+		const tokens = [];
+		for (let i = 0; i < rawTokens.length; i++) {
+			if (!rawTokens[i]) {
+				continue;
+			}
+			switch(rawTokens[i]) {
+				case '[img]':
+					const url = encodeURI(rawTokens[i + 1]);
+					tokens.push({ type: 'image', key: i, src: url });
+					i += 2;
+					break;
+				case '[b]':
+					tokens.push({ type: 'bold', key: i, text: rawTokens[i + 1] });
+					i += 2;
+					break;
+				case '[i]':
+					tokens.push({ type: 'italic', key: i, text: rawTokens[i + 1] });
+					i += 2;
+					break;
+				case '[spoiler]':
+					tokens.push({ type: 'spoiler', key: i, text: rawTokens[i + 1] });
+					i += 2;
+					break;
+				default:
+					tokens.push({ type: 'normal', key: i, text: rawTokens[i] });
+			}
+		}
+		const contentEls = [];
+		for (let i = 0; i < tokens.length; i++) {
+			const token = tokens[i];
+			let lines = [];
+			switch(token.type) {
+				case 'image':
+					contentEls.push(<img alt="user inserted" key={`${i}`} src={token.src} />);
+					break;
+				case 'bold':
+				case 'italic':
+				case 'normal':
+				case 'spoiler':
+					if (token.text.includes('\n')) {
+						lines = token.text.split('\n');
+						lines.forEach((line, lineIndex) => {
+							contentEls.push(<span key={`${i}-${lineIndex}`} className={token.type}>{line}</span>);
+							if (lineIndex !== lines.length - 1) {
+								contentEls.push(<span key={`space-${i}-${lineIndex}`} className="space" />);
+							}
+						});
+					} else {
+						contentEls.push(<span key={i} className={token.type}>{token.text}</span>);
+					}
+					break;
+				default:
+					// just in case - shouldn't hit this
+					contentEls.push(<span key={i}>{token.text}</span>);
+			}
+		}
+		return contentEls;
 	}
 	render() {	
 		const post = this.state.post;
